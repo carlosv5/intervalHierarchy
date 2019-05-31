852e790c22fad186a11acb1a46f010b0140a2e02
Carlos Vega Garcia <carlos.vega.garcia@ericsson.com>
2019-05-31 15:41:00 +0200

    Refactor: Change inheritance. OpenedClass is father class

--- src/main/java/tdd/intervalHierarchy/OpenedInterval.java
+++ src/main/java/tdd/intervalHierarchy/OpenedInterval.java
@@ -1,20 +1,26 @@
 package tdd.intervalHierarchy;
 
-public class OpenedInterval extends Interval{
+public class OpenedInterval {
+	
+	protected double min;
+	protected double max;
 
 	public OpenedInterval(double min, double max) {
-		super(min,max);
+		this.min = min;
+		this.max = max;
 	}
 
-	public boolean isIntersected(Interval another) {
-		return super.isIntersected(another) || this.isTheSame(another);
+	public boolean isIntersected(OpenedInterval another) {
+		return this.isTheSame(another) || this.isIncluded(another.min) ||
+				this.isIncluded(another.max)||
+				another.isIncluded(this.min);
 	}
 
 	protected boolean isIncluded(double value) {
 		return this.min < value && value < this.max;
 	}
 	
-	public boolean isTheSame(Interval another) {
+	public boolean isTheSame(OpenedInterval another) {
 		return this.min == another.min && this.max == another.max;
 	}
 
