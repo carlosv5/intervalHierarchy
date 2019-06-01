619e68de16cd6ac25fda2f9e7e7f172c2871b314
Carlos Vega Garcia <carlos.vega.garcia@ericsson.com>
2019-06-01 12:16:06 +0200

    Add more tests for start with -infinite intervals

--- src/test/java/tdd/intervalHierarchy/InfiniteStartlIntervalTest.java
+++ src/test/java/tdd/intervalHierarchy/InfiniteStartlIntervalTest.java
@@ -10,6 +10,27 @@ import tdd.builders.OpenedIntervalBuilder;
 public class InfiniteStartlIntervalTest extends TestCase {
 	
 	@Test
+	public void testIsIntersectedInfiniteUntilIntervalsWithOpenCloseInterval() {
+		Interval infiniteUntilInterlval1 = new IntervalBuilder().max(12).maxClosed().build();
+		Interval interval2 = new IntervalBuilder().min(1).max(10).maxClosed().build();
+		assertTrue(infiniteUntilInterlval1.isIntersected(interval2));
+	}
+	
+	@Test
+	public void testIsIntersectedInfiniteUntilIntervalsWithOpenInterval() {
+		Interval infiniteUntilInterlval1 = new IntervalBuilder().max(12).maxClosed().build();
+		Interval interval2 = new IntervalBuilder().min(1).max(10).build();
+		assertTrue(infiniteUntilInterlval1.isIntersected(interval2));
+	}
+	
+	@Test
+	public void testIsIntersectedInfiniteUntilIntervalsWithCloseInterval() {
+		Interval infiniteUntilInterlval1 = new IntervalBuilder().max(12).maxClosed().build();
+		Interval interval2 = new IntervalBuilder().min(1).max(10).minClosed().maxClosed().build();
+		assertTrue(infiniteUntilInterlval1.isIntersected(interval2));
+	}
+	
+	@Test
 	public void testIsIntersectedInfiniteUntilIntervalsInside() {
 		Interval infiniteUntilInterlval1 = new IntervalBuilder().max(15).maxClosed().build();
 		Interval infiniteUntilInterlval2 = new IntervalBuilder().max(10).maxClosed().build();
@@ -22,5 +43,18 @@ public class InfiniteStartlIntervalTest extends TestCase {
 		Interval infiniteUntilInterlval2 = new IntervalBuilder().max(12).maxClosed().build();
 		assertTrue(infiniteUntilInterlval1.isIntersected(infiniteUntilInterlval2));
 	}
-
+	
+	@Test
+	public void testIsIntersectedInfiniteUntilIntervalsWithTheSameFinishPoint() {
+		Interval infiniteUntilInterlval1 = new IntervalBuilder().max(12).maxClosed().build();
+		Interval infiniteUntilInterlval2 = new IntervalBuilder().max(12).maxClosed().build();
+		assertTrue(infiniteUntilInterlval1.isIntersected(infiniteUntilInterlval2));
+	}
+	
+	@Test
+	public void testIsIntersectedInfiniteUntilIntervalsOneHasNegativeValue() {
+		Interval infiniteUntilInterlval1 = new IntervalBuilder().max(12).maxClosed().build();
+		Interval infiniteUntilInterlval2 = new IntervalBuilder().max(-10).maxClosed().build();
+		assertTrue(infiniteUntilInterlval1.isIntersected(infiniteUntilInterlval2));
+	}
 }
