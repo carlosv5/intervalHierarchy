f978f002b0e1c19cfa16e572fd50935d21593c75
Carlos Vega Garcia <carlos.vega.garcia@ericsson.com>
2019-06-01 12:29:27 +0200

    Add more tests for finish with infinite intervals

--- src/test/java/tdd/intervalHierarchy/InfiniteFinishlIntervalTest.java
+++ src/test/java/tdd/intervalHierarchy/InfiniteFinishlIntervalTest.java
@@ -10,10 +10,45 @@ import tdd.builders.OpenedIntervalBuilder;
 public class InfiniteFinishlIntervalTest extends TestCase {
 	
 	@Test
-	public void testIsIntersectedInfiniteUntilIntervalsWithOpenCloseInterval() {
-		Interval infiniteFinishInterlval1 = new IntervalBuilder().min(0).maxClosed().build();
+	public void testIsIntersectedInfiniteFinishIntervalsWithOpenCloseInterval() {
+		Interval infiniteFinishInterlval1 = new IntervalBuilder().min(0).build();
 		Interval interval2 = new IntervalBuilder().min(1).max(10).maxClosed().build();
 		assertTrue(infiniteFinishInterlval1.isIntersected(interval2));
+	}
+	
+	@Test
+	public void testIsIntersectedInfiniteFinishIntervalsWithOpenInterval() {
+		Interval infiniteFinishInterlval1 = new IntervalBuilder().min(0).build();
+		Interval interval2 = new IntervalBuilder().min(1).max(10).build();
+		assertTrue(infiniteFinishInterlval1.isIntersected(interval2));
+	}
+	
+	@Test
+	public void testIsIntersectedInfiniteFinishIntervalsWithCloseInterval() {
+		Interval infiniteFinishInterlval1 = new IntervalBuilder().min(0).build();
+		Interval interval2 = new IntervalBuilder().min(1).max(10).minClosed().maxClosed().build();
+		assertTrue(infiniteFinishInterlval1.isIntersected(interval2));
+	}
+	
+	@Test
+	public void testIsIntersectedInfiniteFinishIntervalsInside() {
+		Interval infiniteFinishInterlval1 = new IntervalBuilder().min(0).build();
+		Interval infiniteFinishInterlval2 = new IntervalBuilder().min(5).build();
+		assertTrue(infiniteFinishInterlval1.isIntersected(infiniteFinishInterlval2));
+	}
+	
+	@Test
+	public void testIsIntersectedInfiniteFinishIntervalsWithTheSameStartPoint() {
+		Interval infiniteFinishInterlval1 = new IntervalBuilder().min(0).minClosed().build();
+		Interval infiniteFinishInterlval2 = new IntervalBuilder().min(0).minClosed().build();
+		assertTrue(infiniteFinishInterlval1.isIntersected(infiniteFinishInterlval2));
+	}
+	
+	@Test
+	public void testIsIntersectedInfiniteFinishIntervalsNegativesValue() {
+		Interval infiniteFinishInterlval1 = new IntervalBuilder().min(-12).build();
+		Interval infiniteFinishInterlval2 = new IntervalBuilder().max(-10).build();
+		assertTrue(infiniteFinishInterlval1.isIntersected(infiniteFinishInterlval2));
 	}
 	
 }
